5153a5a4d6b1cd4279c7d3788d3a2f37fd9c06ee
John Rose <john.rose3@comcast.net>
2023-03-13 07:45:52 +0100

    Create project.php
    This file has one problem where the second layout does not render the grid- something with the whay span and columns work together in attributes

---
+++ templates/project.php
@@ -0,0 +1,30 @@
+<?php
+/* add notes here */
+?>
+<?php snippet('header') ?>
+
+<main class="page-margin">
+  <article>
+    <?php snippet('intro-project') ?>
+    <?php snippet('layouts', ['field' => $page->layout()])  ?>
+
+    <?php foreach ($page->projectlayout()->toLayouts() as $layoutTwo): ?>
+    <section class="grid" id="<?= $layoutTwo->id() ?>">
+      <?php foreach ($layoutTwo->columns() as $column): ?>
+      <div class="column" style="--span:<?= $column->span() ?>">
+        <!-- <div class="blocks"> -->
+        <?= $column->blocks() ?>
+        <!-- </div> -->
+      </div>
+      <?php endforeach ?>
+    </section>
+    <?php endforeach ?>
+
+    <div><?= $page->text()->kirbytext() ?></div>
+
+  </article>
+
+
+</main>
+
+<?php snippet('footer') ?>
