25eec100602119ebdddef975a756082038f7b44f
John Rose <john.rose3@comcast.net>
2023-03-16 07:08:35 +0100

    update project.php 2023_03_16
    This includes the cover image code before attempting to add the img tag and attributes to it

--- templates/project.php
+++ templates/project.php
@@ -6,6 +6,36 @@
 <main class="page-margin">
   <article>
     <?php snippet('intro-project') ?>
+
+    <!-- Add cover image and text if exists -->
+    <!-- Insert a cover image with caption, citation and additional explanatory text -->
+    <?php if ($page->cover()->toFile()->isNotEmpty()): ?>
+    <figure class="cover center <?= $page->cover()->toFile()->imgclass() ?>">
+      <?= $page->cover()->toFile() ?>
+      <?php if ($page->cover()->caption()->isNotEmpty()): ?>
+
+      <figcaption>
+        <?= $page->cover()->toFile()->caption() ?>
+      </figcaption>
+      <?php endif ?>
+      <cite><?= $page->cover()->toFile()->citation() ?></cite>
+
+    </figure>
+    <!-- Add some explanatory text below the figure -->
+    <?php if ($page->cover()->toFile()->coverintro()->isNotEmpty()): ?>
+    <div class="coverintro">
+      <?= $page->cover()->toFile()->coverintro()->kirbytext() ?>
+    </div>
+    <?php endif ?>
+    <?php endif ?>
+
+    <?php if ($page->introText()->isNotEmpty()): ?>
+    <div class="intro-text">
+      <?= $page->introText()->kirbytext() ?>
+    </div>
+    <?php endif ?>
+
+
     <?php snippet('layouts', ['field' => $page->layout()])  ?>
 
     <?php foreach ($page->projectlayout()->toLayouts() as $layoutTwo): ?>
@@ -20,7 +50,8 @@
     </section>
     <?php endforeach ?>
 
-    <div><?= $page->text()->kirbytext() ?></div>
+    <!-- A default text block not currently used- commented out on the .yml page as well -->
+    <!-- <div><?= $page->text()->kirbytext() ?></div> -->
 
   </article>
 
