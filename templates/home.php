04adaa1ab551ca76433ca6aab510f515ccceef11
John Rose <john.rose3@comcast.net>
2023-03-24 21:00:33 +0100

    Update prior to testing transfer
    Know I added the headline/title option, maybe more

--- templates/home.php
+++ templates/home.php
@@ -1,13 +1,16 @@
 <?php snippet('header-home') ?>
 
 <main class="page-margin">
+  <?php snippet('contact-form-alert') ?>
   <?= $page->intro()->kt() ?>
-  <?= $site->ContactText() ?>
   <ul class="projects">
     <?php foreach ($page->children() as $project): ?>
-    <li class="project-list">
-      <h3 class="project-title"><?= $project->title() ?></h3>
-      <figure class="project-image figure">
+    <li>
+
+      <?php if ($project->headline()->or($project->title())->isNotEmpty()): ?>
+      <h2 class="project-title"><?= $project->headline()->or($project->title()->html()) ?></h2>
+      <?php endif ?>
+      <!-- <figure class="project-image figure">
         <a href="<?= $project->image()->link() ?>">
           <img src="<?= $project->image()->url() ?>" alt="<?= $project->alt() ?>" width="<?= $project->width() ?>"
             height="<?= $project->height() ?>">
@@ -22,8 +25,23 @@
       </figure>
       <div class=" project-text"><?= markdown($project->text()) ?></div>
     </li>
-    <hr>
+    <hr> -->
+      <?php foreach ($project->layout()->children()->toLayouts() as $layout): ?>
+      <section class=" grid" id="<?= $layout->id() ?>">
+        <?php foreach ($layout->columns() as $column): ?>
+        <div class="column" style="--span:<?= $column->span() ?>; --columns:<?= $column->span() ?>">
+          <div class="blocks">
+            <?= $column->blocks() ?>
+          </div>
+        </div>
+        <?php endforeach ?>
+      </section>
+      <?php endforeach ?>
+      <hr>
+    </li>
     <?php endforeach ?>
+
+
   </ul>
 </main>
 
