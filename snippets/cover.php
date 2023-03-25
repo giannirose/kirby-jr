84fc72548333eda9c32a196b313e2829f6ac3d55
John Rose <john.rose3@comcast.net>
2023-03-25 06:31:18 +0100

    Update cover.php
    cover.php in snippets at time of taking public projects live

--- snippets/cover.php
+++ snippets/cover.php
@@ -1,18 +1,20 @@
 <?php
 
-if($image = $page->toFile()): ?>
-<figure class="<?= $image->figclass() ?> <?= $image->image-position() ?> testclass">
-  <a href="<?= $image->link() ?>">
-    <img class="<?= $image->imgclass() ?> covertestclass" src=" <?= $image->url() ?>" alt="<?= $image->alt() ?>"
-      style="width:<?= $image->imwidth() ?>">
-  </a>
-  <?php if ($image->caption()->isNotEmpty()): ?>
+if($cover = $page->cover()->toFile()): ?>
+<figure class=" site-snippets <?= $cover->figclass() ?>">
+  <img class=" johnclass" src="<?= $cover->url() ?>" alt="<?= $cover->alt() ?>" loading="lazy"
+    width="<?= $cover->width() ?>" height="<?= $cover->height() ?>" />
+
+  <?php if ($cover->caption()->isNotEmpty()): ?>
   <figcaption>
-    <p><?= $image->caption() ?></p>
+    <?= $cover->caption() ?>
   </figcaption>
   <?php endif ?>
-  <?php if ($image->citation()->isNotEmpty()): ?>
-  <cite><?= $image->citation() ?></cite>
+  <?php if ($cover->citation()->isNotEmpty()): ?>
+  <cite><?= $cover->citation() ?></cite>
+
   <?php endif ?>
+
 </figure>
+<h3><?php $cover->cover()->toFile()->coverintro() ?></h3>
 <?php endif ?>
