68dae2ccf409f37e4281b6172efa8e1bcaf627c4
John Rose <john.rose3@comcast.net>
2023-03-25 06:27:52 +0100

    Update gallery.php
    gallery.php in blocks at time of taking public projects live

--- snippets/blocks/gallery.php
+++ snippets/blocks/gallery.php
@@ -6,25 +6,27 @@ $ratio   = $block->ratio()->or('auto');
 
 ?>
 <figure class="noborder" <?= Html::attr(['data-ratio' => $ratio, 'data-crop' => $crop], null, ' ') ?>>
-  <ul class="grid-gallery">
+  <!-- change ul to div to permit subgrid on images and captions -->
+  <div class="grid-gallery">
     <?php foreach ($block->images()->toFiles() as $image): ?>
-    <li class=" grid-list <?= $block->listclass() ?>" style="grid-column: span <?= $image->spanwidth() ?>;" ?>
-      <figure class=" grid-figure <?= $image->figclass() ?>">
-        <a href="<?= $image->url() ?>">
-          <img class="<?= $image->imgclass() ?>" src="<?= $image->resize()->url() ?>" alt="<?= $image->alt() ?>"
-            width="<?= $image->width() ?>" height="<?= $image->height() ?>">
-        </a>
+    <!-- delete list to permit subgrid on figure element -->
+    <!-- <li class=" grid-list <?= $block->listclass() ?>" style="grid-column: span <?= $image->spanwidth() ?>;" > -->
+    <figure class=" grid-figure <?= $image->figclass() ?>" style="grid-column: span <?= $image->spanwidth() ?>;">
+      <a href="<?= $image->url() ?>">
+        <img class="<?= $image->imgclass() ?>" src="<?= $image->resize()->url() ?>" alt="<?= $image->alt() ?>"
+          width="<?= $image->width() ?>" height="<?= $image->height() ?>" loading="lazy">
+      </a>
 
-        <figcaption>
-          <?= $image->caption() ?>
-        </figcaption>
-        <cite>
-          <?= $image->citation() ?>
-        </cite>
-      </figure>
-    </li>
+      <figcaption>
+        <?= $image->caption() ?>
+      </figcaption>
+      <cite>
+        <?= $image->citation() ?>
+      </cite>
+    </figure>
+    <!-- </li> -->
     <?php endforeach ?>
-  </ul>
+  </div>
   <?php if ($caption->isNotEmpty()): ?>
   <figcaption>
     <?= $caption ?>
