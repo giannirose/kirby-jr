2c335d0ada6440def8ec8a27758e7e40a9f50742
John Rose <john.rose3@comcast.net>
2023-04-08 17:30:16 -0700

    snippets blog from card-style page
    This was the card-style listing of blogs saved before changing it to a simple list of title and text

--- snippets/blog.php
+++ snippets/blog.php
@@ -3,27 +3,39 @@
 Snippet is on blogs.php as a link to the blog itself
 */
 ?>
-<article class="blog-excerpt">
+
+
+
+
+<h2 class="blog-excerpt-title"> <a href="<?= $blog->url() ?>"><?= $blog->title()->kirbytext() ?> </a></h2>
+
+
+<div class="blog-excerpt-figure">
   <a href="<?= $blog->url() ?>">
-    <header>
-      <h2 class="blog-excerpt-title"><?= $blog->title()->kirbytext() ?></h2>
-      <figure class="blog-excerpt-figure img" style="--w: 16; --h:9">
-        <?php if ($cover = $blog->cover()): ?>
-        <?= $cover->crop(484, 768); ?>
-        <?php endif ?>
-      </figure>
-
-
-      <div class="blog-excerpt-date"><time class="note-excerpt-date"
-          datetime="<?= $blog->published('c') ?>"><?= $blog->published() ?></time></div>
-    </header>
+    <figure class="blog-excerpt-figure img" style="--w: 16; --h:9">
+      <?php if ($cover = $blog->cover()): ?>
+      <?= $cover->crop(484, 768); ?>
+      <?php endif ?>
+    </figure>
   </a>
+</div>
+<div class="blog-excerpt-date">
+  <a href="<?= $blog->url() ?>">
+    <time class="blog-excerpt-date" datetime="<?= $blog->published('c') ?>"><?= $blog->published() ?></time>
+  </a>
+</div>
+<div class="blog-excerpt-text">
+  <?php if ($blog->excerpt_self()->isNotEmpty()): ?>
+  <?= $blog->excerpt_self() ?>
+  <br>
+  <a class="floatright" href="<?= $blog->url() ?>">Read More...</a>
+  <?php else : ?>
   <?php if (($excerpt ?? true) !== false): ?>
-  <div class="blog-excerpt-text">
-    <?= $blog->intro()->toBlocks()->excerpt(280) ?>
-    <br>
-    <a class="floatright" href="<?= $blog->url() ?>">Read More...</a>
-  </div>
-  <?php endif ?>
 
-</article>
+
+  <?= $blog->intro()->toBlocks()->excerpt(180) ?>
+  <br>
+  <a class="floatright" href="<?= $blog->url() ?>">Read More...</a>
+  <?php endif ?>
+  <?php endif ?>
+</div>
