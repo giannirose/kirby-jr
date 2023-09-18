bb795453905ca96319d84834a4274f9add547e60
John Rose <john.rose3@comcast.net>
2023-09-18 06:17:13 -0700

    Update layouts.php 2023_09_18
    Basic layouts arrangement with blocks, most likely from Kirby docs

--- snippets/layouts.php
+++ snippets/layouts.php
@@ -1,23 +1,9 @@
-<?php
-/*
-  Snippets are a great way to store code snippets for reuse
-  or to keep your templates clean.
-
-  This layouts snippet renders the content of a layout
-  field with our custom grid system.
-
-  More about snippets:
-  https://getkirby.com/docs/guide/templates/snippets
-*/
-?>
 <?php foreach ($field->toLayouts() as $layout): ?>
-<section class="margin-xl grid" id="<?= $layout->id() ?>" style="--gutter: 1.5rem">
-  <?php foreach ($layout->columns() as $column): ?>
-  <div class="column" style="--columns:<?= $column->span() ?>">
-
-    <?= $column->blocks() ?>
-  </div>
-
-  <?php endforeach ?>
-</section>
+  <section class="margin-xl grid" id="<?= $layout->id() ?>" style="--gutter: 2.5rem">
+    <?php foreach ($layout->columns() as $column): ?>
+      <div class="column" style="--columns:<?= $column->span() ?>">
+        <?= $column->blocks() ?>
+      </div>
+    <?php endforeach ?>
+  </section>
 <?php endforeach ?>
