e705dc858d5de0de32307b33fa87939831159c3a
John Rose <john.rose3@comcast.net>
2024-01-13 08:09:28 -0800

    Update header-blog.php 2024_01_13
    Streamline layout

--- snippets/header-blog.php
+++ snippets/header-blog.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html>
 
 <head>
@@ -5,12 +6,18 @@
   <meta name="description" content="<?= $site->description() ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>
-    <?= $page->title() ?> | <?= $site->title() ?>
+    <?= $page->title() ?> |
+    <?= $site->title() ?>
   </title>
   <?= css('assets/css/starter.css') ?>
   <?= css('assets/css/index.css') ?>
+  <?= css('assets/css/blog.css') ?>
+  <?= css('assets/css/blogs.css') ?>
   <?= css('@auto') ?>
+  <?= css('assets/css/headerblog.css') ?>
+
 </head>
+<!-- header-blog does not have the full navigation -->
 
 <body>
   <header class="header-main">
@@ -19,12 +26,15 @@
     </div>
     <div class="header-spacer"></div>
     <div class="header-logo-box">
-      <img class="header-logo" src="<?= $site->image()->url() ?>" alt="layered orange fused glass disc with bubbles">
+      <img class="header-logo" src="<?= $site->image()->url() ?>"
+        alt="trees in the mountains and trees in the valley and a comet in stained glass">
     </div>
     <div class="header-text">
-      <h2 class="page-title">
-        <?= $site->titleBlog() ?>
-      </h2>
+      <a href="/blogs">
+        <h2 class="page-title">
+          <?= $site->titleBlog() ?>
+        </h2>
+      </a>
       <p class="page-subtitle gradient-text">
         <?= $site->straplineBlog() ?>
       </p>
@@ -32,23 +42,28 @@
     <div class="header-bottom">
     </div>
     <nav class="header-nav">
-      <ul class="nav-main">
+      <ul class="nav-main nav-colors">
         <!-- Limit the blog links to the stained glass home page -->
         <li>
-          <a href="<?= $site->url() ?>"><?= $site->title() ?></a>
+          <a href="<?= $site->url() ?>">
+            <!-- <?= $site->title() ?> -->
+            Stained<br>Glass
+          </a>
         </li>
-        <!-- The following would be used for a listing of all pages -->
-        <!--       <?php foreach ($site->children()->listed() as $item): ?>
         <li>
-          <a href="<?= $item->url() ?>"><?= $item->title()
-           ?></a>
+          <a href="/blogs">
+            Crafty:<br>
+            The blog
+          </a>
         </li>
-        <?php endforeach ?>
-        <li><a href="<?= $item->url() ?>"><?= $site->children()->nth(1)->title(); ?>
+
+        <li><a href="/Contact">
+            Contact
           </a></li>
- -->
+
 
       </ul>
+
     </nav>
 
 
