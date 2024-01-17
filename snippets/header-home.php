c6772735e6af7158a29b91c5735070dbd29f05e2
John Rose <john.rose3@comcast.net>
2024-01-17 09:06:46 -0800

    Update header-home.php

--- snippets/header-home.php
+++ snippets/header-home.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html>
 
 <head>
@@ -5,23 +6,26 @@
   <meta name="description" content="<?= $site->description() ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>
-    <?= $page->title() ?> | <?= $site->title() ?>
+    <?= $page->title() ?> |
+    <?= $site->title() ?>
   </title>
+  <?= css('assets/css/starter.css') ?>
   <?= css('assets/css/index.css') ?>
   <?= css('@auto') ?>
 </head>
+<!-- snippet header-home has an h1 tag on the page title -->
 
-<body>
+<body class="<?= $page->navclass() ?>">
   <header class="header-main">
 
     <div class="header-top">
     </div>
     <div class="header-spacer"></div>
-    <div class="header-logo-box">
-      <img class="header-logo" src="<?= $site->image()->url() ?>" alt="layered orange fused glass disc with bubbles">
-    </div>
+
     <div class="header-text">
+
       <h1 class="page-title">
+
         <?= $site->title() ?>
       </h1>
       <p class="page-subtitle gradient-text">
@@ -30,15 +34,20 @@
     </div>
     <div class="header-bottom">
     </div>
-    <nav class="header-nav">
+    <!-- the main navigation is set by dates or numbers underscore on individual content files -->
+    <nav class="header-nav" id="menu">
       <ul class="nav-main nav-colors">
         <?php foreach ($site->children()->listed() as $item): ?>
-        <li>
-          <a href="<?= $item->url() ?>"><?= $item->title() ?></a>
-        </li>
+          <li class="<?= $item->navclass() ?>">
+            <a href="<?= $item->url() ?>">
+              <?= $item->title() ?>
+            </a>
+          </li>
         <?php endforeach ?>
       </ul>
     </nav>
-
+    <div class="header-logo-box">
+      <img class="header-logo" src="<?= $site->image()->url() ?>" alt="layered orange fused glass disc with bubbles">
+    </div>
 
   </header>
